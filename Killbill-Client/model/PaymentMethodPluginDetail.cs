306e4d2773ac64bafec699f5ea256e321b9ac0bb
Justice Ogbonna <ebesco@yahoo.com>
2016-07-11 18:45:05 -0400

    [non-working] migration of java sources to c#

--- Killbill-Client/model/PaymentMethodPluginDetail.cs
+++ Killbill-Client/model/PaymentMethodPluginDetail.cs
@@ -1,27 +1,9 @@
-/*
- * Copyright 2010-2014 Ning, Inc.
- * Copyright 2015 Groupon, Inc
- * Copyright 2015 The Billing Project, LLC
- *
- * The Billing Project licenses this file to you under the Apache License, version 2.0
- * (the "License"); you may not use this file except in compliance with the
- * License.  You may obtain a copy of the License at:
- *
- *    http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
- * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
- * License for the specific language governing permissions and limitations
- * under the License.
- */
+
+using System;
+using System.Collections.Generic;
+using System.Text;
 
 namespace Killbill_Client.model {
-
-
-
-
-
 
 public class PaymentMethodPluginDetail {
 
@@ -29,69 +11,36 @@ public class PaymentMethodPluginDetail {
     private bool isDefaultPaymentMethod;
     private List<PluginProperty> properties;
 
-    public PaymentMethodPluginDetail() {}
-
     
-    public PaymentMethodPluginDetail(@JsonProperty("externalPaymentMethodId") string externalPaymentMethodId,
-                                     @JsonProperty("isDefaultPaymentMethod") bool isDefaultPaymentMethod,
-                                     @JsonProperty("properties") List<PluginProperty> properties) {
-        this.externalPaymentMethodId = externalPaymentMethodId;
-        this.isDefaultPaymentMethod = isDefaultPaymentMethod;
-        this.properties = properties;
-    }
-
-    public string getExternalPaymentMethodId() {
-        return externalPaymentMethodId;
-    }
-
-    public void setExternalPaymentMethodId(string externalPaymentMethodId) {
-        this.externalPaymentMethodId = externalPaymentMethodId;
-    }
-
-    public bool getIsDefaultPaymentMethod() {
-        return isDefaultPaymentMethod;
-    }
-
-    public void setIsDefaultPaymentMethod(bool isDefaultPaymentMethod) {
-        this.isDefaultPaymentMethod = isDefaultPaymentMethod;
-    }
-
-    public List<PluginProperty> getProperties() {
-        return properties;
-    }
-
-    public void setProperties(List<PluginProperty> properties) {
-        this.properties = properties;
-    }
 
     
     public string ToString() {
         StringBuilder sb = new StringBuilder("PaymentMethodPluginDetail{");
-        sb.Append("externalPaymentMethodId='").append(externalPaymentMethodId).append('\'');
-        sb.Append(", isDefaultPaymentMethod=").append(isDefaultPaymentMethod);
-        sb.Append(", properties=").append(properties);
+        sb.Append("externalPaymentMethodId='").Append(externalPaymentMethodId).Append('\'');
+        sb.Append(", isDefaultPaymentMethod=").Append(isDefaultPaymentMethod);
+        sb.Append(", properties=").Append(properties);
         sb.Append('}');
         return sb.ToString();
     }
 
     
-    public bool equals(Object o) {
+    public bool Equals(Object o) {
         if (this == o) {
             return true;
         }
-        if (o == null || getClass() != o.getClass()) {
+        if (o == null || GetType() != o.GetType()) {
             return false;
         }
 
         PaymentMethodPluginDetail that = (PaymentMethodPluginDetail) o;
 
-        if (externalPaymentMethodId != null ? !externalPaymentMethodId.equals(that.externalPaymentMethodId) : that.externalPaymentMethodId != null) {
+        if (externalPaymentMethodId != null ? !externalPaymentMethodId.Equals(that.externalPaymentMethodId) : that.externalPaymentMethodId != null) {
             return false;
         }
-        if (isDefaultPaymentMethod != null ? !isDefaultPaymentMethod.equals(that.isDefaultPaymentMethod) : that.isDefaultPaymentMethod != null) {
+        if (isDefaultPaymentMethod != null ? !isDefaultPaymentMethod.Equals(that.isDefaultPaymentMethod) : that.isDefaultPaymentMethod != null) {
             return false;
         }
-        if (properties != null ? !properties.equals(that.properties) : that.properties != null) {
+        if (properties != null ? !properties.Equals(that.properties) : that.properties != null) {
             return false;
         }
         return true;
--- Killbill-Client/model/Phase.cs
+++ Killbill-Client/model/Phase.cs
@@ -1,112 +1,71 @@
-/*
- * Copyright 2010-2014 Ning, Inc.
- *
- * Ning licenses this file to you under the Apache License, version 2.0
- * (the "License"); you may not use this file except in compliance with the
- * License.  You may obtain a copy of the License at:
- *
- *    http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
- * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
- * License for the specific language governing permissions and limitations
- * under the License.
- */
 
-namespace Killbill_Client.model {
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Killbill_Client.model
+{
+    public class Phase : KillBillObject
+    {
+
+        private string type;
+        private List<Price> prices;
+        private List<Price> fixedPrices;
+        private Duration duration;
+
+        public string ToString()
+        {
+            StringBuilder sb = new StringBuilder("Phase{");
+            sb.Append("type='").Append(type).Append('\'');
+            sb.Append(", prices=").Append(prices);
+            sb.Append(", fixedPrices=").Append(fixedPrices);
+            sb.Append(", duration=").Append(duration);
+            sb.Append('}');
+            return sb.ToString();
+        }
 
 
+        public bool Equals(Object o)
+        {
+            if (this == o)
+            {
+                return true;
+            }
+            if (o == null || GetType() != o.GetType())
+            {
+                return false;
+            }
 
+            Phase phase = (Phase) o;
 
+            if (prices != null ? !prices.Equals(phase.prices) : phase.prices != null)
+            {
+                return false;
+            }
+            if (type != null ? !type.Equals(phase.type) : phase.type != null)
+            {
+                return false;
+            }
+            if (fixedPrices != null ? !fixedPrices.Equals(phase.fixedPrices) : phase.fixedPrices != null)
+            {
+                return false;
+            }
+            if (duration != null ? !duration.Equals(phase.duration) : phase.duration != null)
+            {
+                return false;
+            }
 
-
-public class Phase : KillBillObject {
-
-    private string type;
-    private List<Price> prices;
-    private List<Price> fixedPrices;
-    private Duration duration;
-
-    
-    public Phase(@JsonProperty("type") string type,
-                 @JsonProperty("prices") List<Price> prices,
-                 @JsonProperty("fixedPrices") List<Price> fixedPrices,
-                 @JsonProperty("duration") Duration duration) {
-        this.type = type;
-        this.prices = prices;
-        this.fixedPrices = fixedPrices;
-        this.duration = duration;
-    }
-
-    public string getType() {
-        return type;
-    }
-
-    public void setType(string type) {
-        this.type = type;
-    }
-
-    public List<Price> getPrices() {
-        return prices;
-    }
-
-    public void setPrices(List<Price> prices) {
-        this.prices = prices;
-    }
-
-    public List<Price> getFixedPrices() {
-        return fixedPrices;
-    }
-
-    public Duration getDuration() {
-        return duration;
-    }
-
-    
-    public string ToString() {
-        StringBuilder sb = new StringBuilder("Phase{");
-        sb.Append("type='").append(type).append('\'');
-        sb.Append(", prices=").append(prices);
-        sb.Append(", fixedPrices=").append(fixedPrices);
-        sb.Append(", duration=").append(duration);
-        sb.Append('}');
-        return sb.ToString();
-    }
-
-    
-    public bool equals(Object o) {
-        if (this == o) {
             return true;
         }
-        if (o == null || getClass() != o.getClass()) {
-            return false;
-        }
 
-        Phase phase = (Phase) o;
 
-        if (prices != null ? !prices.equals(phase.prices) : phase.prices != null) {
-            return false;
+        public int GetHashCode()
+        {
+            int result = type != null ? type.GetHashCode() : 0;
+            result = 31*result + (prices != null ? prices.GetHashCode() : 0);
+            result = 31*result + (fixedPrices != null ? fixedPrices.GetHashCode() : 0);
+            result = 31*result + (duration != null ? duration.GetHashCode() : 0);
+            return result;
         }
-        if (type != null ? !type.equals(phase.type) : phase.type != null) {
-            return false;
-        }
-        if (fixedPrices != null ? !fixedPrices.equals(phase.fixedPrices) : phase.fixedPrices != null) {
-            return false;
-        }
-        if (duration != null ? !duration.equals(phase.duration) : phase.duration != null) {
-            return false;
-        }
-
-        return true;
-    }
-
-    
-    public int GetHashCode() {
-        int result = type != null ? type.GetHashCode() : 0;
-        result = 31 * result + (prices != null ? prices.GetHashCode() : 0);
-        result = 31 * result + (fixedPrices != null ? fixedPrices.GetHashCode() : 0);
-        result = 31 * result + (duration != null ? duration.GetHashCode() : 0);
-        return result;
     }
 }
